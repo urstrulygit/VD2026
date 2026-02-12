f55c26d6cdd0031e81ea13a901a3be7a25a19aa1
urstrulygit <urstrulybharath1@gmail.com>
2026-02-11 19:56:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dzbze8owq/video/upload/v1770857677/Ed_Sheeran_-_Perfect_Duet_with_Beyonc%C3%A9_Official_Audio_kylynl.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
